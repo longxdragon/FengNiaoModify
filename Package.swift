f46890fe807aa665d83a3231d8b30ebfc75eb886
gkoudai_xulong <longxdragon@163.com>
2017-07-27 11:36:04 +0800

    Add FengNiaoModiftKit

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "FengNiaoModify",
-    
+    targets: [
+        Target(name: "FengNiaoModifyKit", dependencies: []),
+        Target(name: "FengNiaoModify", dependencies: ["FengNiaoModifyKit"])
+    ],
     dependencies: [
         .Package(url: "https://github.com/jatoben/CommandLine.git", "3.0.0-pre1"),
         .Package(url: "https://github.com/onevcat/Rainbow.git", majorVersion: 2)
